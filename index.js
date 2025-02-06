da6dbf81ab9707aa2e129df38712d57bcf8b2acd
Ajjaj Ahmed <ajjajahmedasif@gmail.com>
2025-02-06 22:18:22 +0600

    adding single jobs get

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(express.json())
 app.use(cors())
@@ -38,6 +38,13 @@ async function run() {
       const cursor = jobsCollection.find();
       const result = await cursor.toArray();
       res.send(result)
+    })
+
+    app.get('/jobs/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)}
+      const result= await jobsCollection.findOne(query);
+      res.send(result);
     })
 
   } finally {
